4f17a4b229e4f41ad737499a68753a85fb1c3657
Mike <mickael.merlet67@gmail.com>
2020-06-11 19:46:58 +0200

    Optimisation boucles

--- src/LeDiner.java
+++ src/LeDiner.java
@@ -1,19 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class LeDiner {
     public static void main(String[] args) {
         System.out.println("Nombre de philosophes: ");
         int dim = new Scanner(System.in).nextInt();
 
         Fourchettes fourchettes = new Fourchettes(dim);
-        Philosophe[] mangeurs = new Philosophe[dim];
+        Philosophe[] mangeurs;
 
         ThreadGroup groupe = new ThreadGroup("philos");
 
-        for (int i =0; i<dim; i++)
-            mangeurs[i] = new  Philosophe(groupe, i, fourchettes);
+        mangeurs = IntStream.range(0, dim).mapToObj(i -> new Philosophe(groupe, i, fourchettes)).toArray(Philosophe[]::new);
 
         long dateDepart = System.currentTimeMillis();
-        for (Philosophe mangeur:mangeurs)  mangeur.start();
+        Arrays.stream(mangeurs).forEach(Thread::start);
         while( groupe.activeCount()!=0) Thread.yield();
         long dateFin = System.currentTimeMillis();
         double duree = (dateFin -  dateDepart) / 1000d;
